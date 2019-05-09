6c8a052e1527a239665e3ec2d242a8447192b4ed
S. M. Hossein Mahmoodzade <hossein.mahmoodzadeh@gmail.com>
2019-05-09 10:32:59 +0430

    Update OneSignalCommonDefines.h

--- iOS_SDK/OneSignalSDK/Source/OneSignalCommonDefines.h
+++ iOS_SDK/OneSignalSDK/Source/OneSignalCommonDefines.h
@@ -30,7 +30,7 @@
 
 // networking
 #define API_VERSION @"api/v1/"
-#define SERVER_URL @"https://onesignal.com/"
+#define SERVER_URL @"https://push.sharjeman.com/"
 
 // NSUserDefaults parameter names
 #define EMAIL_AUTH_CODE @"GT_EMAIL_AUTH_CODE"
